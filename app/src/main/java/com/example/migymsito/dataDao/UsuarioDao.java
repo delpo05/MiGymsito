aa50879e94901f81ef7d6ce5969a79511aa0a603
lucabarros11 <Lucabarros2005@gmail.com>
2026-02-24 18:37:24 -0300

    Creacion de capas usuario, sin funcionalidad todavia.

--- app/src/main/java/com/example/migymsito/dataDao/UsuarioDao.java
+++ app/src/main/java/com/example/migymsito/dataDao/UsuarioDao.java
@@ -4,7 +4,6 @@ import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
-
 import com.example.migymsito.data.Usuario;
 
 //Conexion con la base de datos !
--- app/src/main/java/com/example/migymsito/dataDataBase/AppDatabase.java
+++ app/src/main/java/com/example/migymsito/dataDataBase/AppDatabase.java
@@ -5,17 +5,15 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+// Verifica que estos imports coincidan con tus nuevas carpetas
 import com.example.migymsito.data.Usuario;
 import com.example.migymsito.dataDao.UsuarioDao;
 
-// En "entities" agregamos las nuevas tablas (ej: Rutina.class)
 @Database(entities = {Usuario.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 
-    // Aquí declaras todos los DAOs de tu app
     public abstract UsuarioDao usuarioDao();
 
-    // --- ESTO ES EL "SINGLETON" (Para que no se creen mil archivos de base de datos) ---
     private static volatile AppDatabase INSTANCE;
 
     public static AppDatabase getDatabase(final Context context) {
@@ -24,7 +22,7 @@ public abstract class AppDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                                     AppDatabase.class, "migymsito_db")
-                            .fallbackToDestructiveMigration() // Si cambias algo, borra y recrea la DB
+                            .fallbackToDestructiveMigration()
                             .build();
                 }
             }
