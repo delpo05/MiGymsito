115219f434e5965507c37e73b129adecd1f5b11e
lucabarros11 <Lucabarros2005@gmail.com>
2026-02-23 22:18:42 -0300

    feat: add Luca and lautycornu to the member list in main activity
    Realizamos todas las capas para utilizar la clase o tabla usuario.
    Lo dividimos en subcarpetas pero faltaron detalles: como ver los nombres de las funciones
    importar las otras clases que se encuentran en subcarpetas, etc.

---
+++ app/src/main/java/com/example/migymsito/data/Usuario.java
@@ -0,0 +1,40 @@
+package com.example.migymsito.data;
+
+//importacion de librerias
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+
+//definicion de la estructura de la tabla Usuario
+@Entity(tableName = "Usuario",
+        indices = {
+                @Index(value = {"correoElectronicoUsuario"}, unique = true),
+                @Index(value = {"nombreUsuario"}, unique = true)
+                  }
+)
+
+public class Usuario {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @NonNull
+    public String nombreUsuario;
+
+    @NonNull
+    public String correoElectronicoUsuario;
+
+    @NonNull
+    public Long fechaNacimiento;
+
+    @NonNull
+    public String generoUsuario;
+
+    @NonNull
+    public String contraseniaUsuario;
+
+}
+
+
+
---
+++ app/src/main/java/com/example/migymsito/dataDao/UsuarioDao.java
@@ -0,0 +1,34 @@
+package com.example.migymsito.dataDao;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.migymsito.data.Usuario;
+
+//Conexion con la base de datos !
+
+@Dao
+public interface UsuarioDao {
+
+    // REGISTRAR USUARIO: Guarda al usuario con todos sus campos
+    // En caso de existir un usuario con el mismo correo, no lo guarda, para esto usa el
+    // onConflict ABORT (para no guardarlo) por el "unique" que pusiste.
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    void registrarUsuario(Usuario usuario);
+
+    // 2. LOGIN: Busca un usuario que coincida con el correo Y la contraseña.
+    // Si encuentra uno, te devuelve el objeto Usuario. Si no, devuelve null.
+    @Query("SELECT * FROM Usuario WHERE correoElectronicoUsuario = :correo AND contraseniaUsuario = :password LIMIT 1")
+    Usuario login(String correo, String password);
+
+    // 3. VERIFICAR CORREO: Por si solo quieres saber si el correo ya existe antes de registrar.
+    @Query("SELECT * FROM Usuario WHERE correoElectronicoUsuario = :correo LIMIT 1")
+    Usuario validarCorreoUsuario(String correo);
+
+    // 4. VERIFICAR NOMBRE: Para saber si el nombre ya está ocupado antes de registrar
+    @Query("SELECT * FROM Usuario WHERE nombreUsuario = :nombre LIMIT 1")
+    Usuario validarNombreUsuario(String nombre);
+
+}
