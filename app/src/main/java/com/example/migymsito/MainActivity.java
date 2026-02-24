0ddb85efd283f5863fdac612b5fb1de14c1ecd82
juaanlopez <juaannpablo@outlook.com>
2026-02-23 21:50:34 -0300

    Diseñe pagina inicio sesion queda mejorarlo todavia

--- app/src/main/java/com/example/migymsito/MainActivity.java
+++ app/src/main/java/com/example/migymsito/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.migymsito;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText etUsuario, etPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        
+        etUsuario = findViewById(R.id.etUsuario);
+        etPassword = findViewById(R.id.etPassword);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void EventoBoton(View view) {
+        String usuario = etUsuario.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if (usuario.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Por favor complete todos los campos", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Bienvenido " + usuario, Toast.LENGTH_SHORT).show();
+        }
     }
 }
