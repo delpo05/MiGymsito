6e36ab5e1e1e632aa4f47a2d5b94146c8db9f6c1
lucabarros11 <Lucabarros2005@gmail.com>
2026-03-15 16:00:54 -0300

    Refactorizar DatosPersonalesActivity y su ubicación en el proyecto.
    - Mover `DatosPersonalesActivity.java` del paquete `dataRepository` al directorio principal de la aplicación.
    - Cambiar la clase base de `HeaderActivity` a `AppCompatActivity`.
    - Agregar el import necesario para `androidx.appcompat.app.AppCompatActivity`.

--- app/src/main/java/com/example/migymsito/DatosPersonalesActivity.java
+++ app/src/main/java/com/example/migymsito/DatosPersonalesActivity.java
@@ -1,13 +1,14 @@
 package com.example.migymsito;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class DatosPersonalesActivity extends HeaderActivity {
+
+public class DatosPersonalesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.datos_personales);
     }
 }
